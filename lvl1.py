8a7658ea965d9c1215f23442a95109151158633f
Trxppy <72364947+TrxppyVX@users.noreply.github.com>
2021-11-04 14:56:52 -0400

    Create lvl1.py

---
+++ lvl1.py
@@ -0,0 +1,8 @@
+#---------------------------------File Imports---------------------------------#
+import pygame
+import pygame.locals
+impoty classes
+#---------------------------------Pygame Initiation---------------------------------#
+pygame.init()
+#---------------------------------Pygame Lvl-1---------------------------------#
+#we need objects and a goal
